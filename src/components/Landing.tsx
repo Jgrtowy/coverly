25f5ea33f89cb70fd9fe8b67996f68f2aa6b2568
Jgrtowy <panjogurcik@outlook.com>
2025-11-13 18:39:13 +0100

    style: Add landing page

---
+++ src/components/Landing.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+export default function Landing() {
+    return (
+        <main className="dark:text-foreground text-background w-screen h-dvh flex flex-col justify-center items-center relative gap-4">
+            <div className="flex gap-4 items-center">
+                <Image
+                    src="/icon.svg"
+                    alt="Coverly Logo"
+                    width={96}
+                    height={96}
+                />
+                <h1 className="text-6xl font-black">Coverly</h1>
+            </div>
+            <h2 className="text-2xl font-bold italic">
+                Generate beautiful posters from your favorite albums in just a
+                few clicks!
+            </h2>
+            <Button className="text-xl w-72 h-16 cursor-pointer">
+                Continue with Last.fm
+            </Button>
+        </main>
+    );
+}
